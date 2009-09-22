1d5d141ce663e2042fe9c2db024f457950abb8a9
Nilesh Kapadia <git@nileshk.com>
2009-09-22 00:46:54 -0400

    Get table names from database (metadata)

--- velocity/src/main/clojure/script.clj
+++ velocity/src/main/clojure/script.clj
@@ -6,6 +6,33 @@
 (import '(java.io 
           BufferedWriter FileOutputStream OutputStream OutputStreamWriter))
 (import '(java.util Date))
+(import '(java.sql
+          Connection DatabaseMetaData ResultSet DriverManager))
+
+(defn database-connect "Connect to database" []
+  (Class/forName "org.postgresql.Driver")
+  (DriverManager/getConnection "jdbc:postgresql://localhost/todo" "nil" "nil"))
+
+(defn table-metadata
+  "Get database table metadata"
+  [connection schema table]
+  (def metaData (. connection getMetaData))
+  (def rs (. metaData getTables nil schema table 
+             (to-array ["TABLE"]))))
+
+(defn table-names
+  "Get table names"
+  [connection schema]
+  (def metaData (. connection getMetaData))
+  (let [rs (. metaData getTables nil schema nil (into-array ["TABLE"]))]
+    (loop []
+      (when (.next rs)
+        ;; TODO return a sequence instead of printing
+        (println (.getString rs "TABLE_NAME"))
+        (recur)))))
+
+(let [connection (database-connect)]
+  (table-names connection "public"))
 
 (def outputFileName "outfile.txt")
 (def templateFileName "test.vm")
