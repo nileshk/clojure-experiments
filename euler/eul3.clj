3d90820583fc098ca537f87846918d7cc22688b7
Nilesh Kapadia <code@nileshk.com>
2009-10-20 23:26:26 -0400

    Improvements to Euler #3 (solution still not fast enough)

--- euler/eul3.clj
+++ euler/eul3.clj
@@ -1,18 +1,24 @@
 ;; Euler #3 doesn't work yet
-(defn prime? [n]
-  (if (or (= 1 n) (= 2 n))
-    true
-    (loop [d (- n 1)]
-      (if (= d 1)
-        true
-        (if (= 0 (mod n d))
-          false
-          (recur (dec d)))))))
-
 (defn half-of [n]
   (if (zero? (mod n 2))
     (/ n 2)
     (/ (- n 1) 2)))
+
+(defn half-of-odd [n]
+  (let [h (half-of n)]
+    (if (even? h) (inc h) h)))
+
+(defn prime? [n]
+  (if (or (= 1 n) (= 2 n))
+    true
+    (if (even? n)
+      false
+      (loop [d (half-of-odd n)]
+        (if (= d 1)
+          true
+          (if (= 0 (mod n d))
+            false
+            (recur (- d 2))))))))
 
 (defn eul3 [number]
   (loop [n (half-of number)]
