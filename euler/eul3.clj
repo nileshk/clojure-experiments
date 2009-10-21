596d16d5f68ffc45008aca6e406c42510e0e48fb
Nilesh Kapadia <code@nileshk.com>
2009-10-21 00:24:01 -0400

    Another failed attempt at Euler #3

--- euler/eul3.clj
+++ euler/eul3.clj
@@ -9,24 +9,40 @@
     (if (even? h) (inc h) h)))
 
 (defn prime? [n]
-  (if (or (= 1 n) (= 2 n))
-    true
-    (if (even? n)
-      false
-      (loop [d (half-of-odd n)]
-        (if (= d 1)
-          true
-          (if (= 0 (mod n d))
-            false
-            (recur (- d 2))))))))
+  (if (= 1 n)
+    false
+    (if (= 2 n)
+      true
+      (if (even? n)
+        false
+        (loop [d (half-of-odd n)]
+          (if (= d 1)
+            true
+            (if (= 0 (mod n d))
+              false
+              (recur (- d 2)))))))))
+
+(def primes-length 2000)
+
+; Vector of 2000 primes
+(def primes (vec (take primes-length (filter prime? (iterate inc 1)))))
+
+(defn next-prime-factor [n]
+  (loop [index 0]
+    (if (= index primes-length)
+      nil
+      (let [prime (primes index)]
+        (if (zero? (mod n prime))
+          prime
+          (recur (inc index)))))))
 
 (defn eul3 [number]
-  (loop [n (half-of number)]
-    (if (and 
-         (zero? (mod number n))
-         (prime? n))
-      n
-      (recur (dec n)))))
+  (loop [n number max-factor 0]
+    (let [prime-factor (next-prime-factor n)]
+      (if (nil? prime-factor)
+        max-factor
+        (recur (/ number prime-factor)
+               (max prime-factor max-factor))))))
 
 (eul3 13195) ;; 29
 (eul3 600851475143)
